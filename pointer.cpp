b7614dcb8c56a05b4d86d41884b2b6c26c557a52
Marco Behnke <marco.behnke@innogames.com>
2019-12-06 16:10:53 +0100

    pointer with freeing memory and accessing it

--- pointer.cpp
+++ pointer.cpp
@@ -11,4 +11,31 @@ int main()
     int a = 10, b = 20;
     print(&a);
     print(&b);
+
+    int *p;
+    p = (int*) malloc(5*sizeof(int));
+
+    if(!p)
+    {
+        cout << "Memory Allocation Failed" << endl;
+        exit(1);
+    }
+
+    p[0] = 10;
+    p[1] = 20;
+    p[2] = 30;
+    p[3] = 40;
+    p[4] = 50;
+
+    for (int i=0; i<5; i++)
+    {
+        /* ptr[i] and *(ptr+i) can be used interchangeably */
+        cout << "Value (a): " << *(p+i) << endl;
+        cout << "Value (b): " << p[i] << endl;
+    }
+
+    free(p);
+    p = new int;
+
+    cout << "Org values: (" << a << ", " << b << ")" << endl;
 }
